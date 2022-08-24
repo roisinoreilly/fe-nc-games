4a7c487b142df68a3cf803dc06ff16e8bbd95f8c
Roisin O'Reilly <roisin.oreilly1996@gmail.com>
2022-08-24 12:30:03 +0100

    add updated votes component

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -11,22 +11,39 @@ export const Votes = ({ review }) => {
     setUpvotes((currentVotes) => currentVotes + 1);
     increaseVotes(review.review_id, 1);
     setHasVotedUp(true)
+    setHasVotedDown(false)
+    document.getElementById("upvote-button").disabled = true
+    document.getElementById("downvote-button").disabled = false
     }
   };
 
   const downvote = () => {
-    if (hasVotedDown === false) {
+    if (hasVotedDown === false && hasVotedUp === false && upvotes >0) {
     setUpvotes((currentVotes) => currentVotes - 1);
     increaseVotes(review.review_id, -1);
+    document.getElementById("downvote-button").disabled = true
     setHasVotedDown(true)
+    document.getElementById("upvote-button").disabled = false
+    }
+
+    if (hasVotedDown === false && hasVotedUp && upvotes >0) {
+        setUpvotes((currentVotes) => currentVotes - 1);
+    increaseVotes(review.review_id, -1);
+    setHasVotedDown(false)
+    setHasVotedUp(false)
+    document.getElementById("upvote-button").disabled = false
+    document.getElementById("downvote-button").disabled = false
+    }
+    if (upvotes <=0) {
+        document.getElementById("downvote-button").disabled = true
     }
   };
 
   return (
     <div>
-      <button onClick={upvote}>upvote</button>
-      <button onClick={downvote}>downvote</button>
-      <div>Votes: {upvotes}</div>
+      <button id="upvote-button" onClick={upvote}>upvote</button>
+      <button id="downvote-button" onClick={downvote}>downvote</button>
+      <p>Votes: {upvotes}</p>
     </div>
   );
 };
