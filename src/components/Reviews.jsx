f5956dcb21c6f20a9ce4abcfe868f7030ed3af68
Roisin O'Reilly <roisin.oreilly1996@gmail.com>
2022-08-26 14:13:48 +0100

    add sort component

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,10 +1,13 @@
 import { getAllReviews } from "../utils/api";
 import { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { AllReviews } from "./AllReviews";
 
 export const Reviews = () => {
   const [allReviews, setAllReviews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortBy, setSortBy] = useState()
+  const [orderBy, setOrderBy] = useState("desc")
 
   let { category_slug } = useParams();
 
@@ -14,43 +17,39 @@ export const Reviews = () => {
       setIsLoading(false);
     });
   }, [category_slug]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    getAllReviews(category_slug, sortBy, orderBy).then((allReviews) => {
+      setAllReviews(allReviews);
+      setIsLoading(false);
+      <AllReviews allReviews={allReviews}/>
+    });
+  }
+
   if (isLoading) {
     return <p className="loading">Loading...</p>;
   } else {
-    return allReviews.map((review) => {
-      return (
-        <>
-        <div className="card" style={{width: "18rem"}}>
-        <img
-            src={review.review_img_url}
-            key={review.id}
-            className="card-img-top"
-            alt={review.title}
-            style={{width: "18rem", height: "10rem", "objectFit": "cover"}}
-          ></img>
-          <div className="card-body">
-            <h5 className="card-title">{review.title}</h5>
-            <p className="card-text">{review.owner}</p>
-            <p className="snippet">{review.review_body}</p>
-            <Link to={`/review/${review.review_id}`}>Read more</Link>
-          </div>
-        </div>
+    return (
+      <>
+        <form id='sort-reviews' onSubmit={handleSubmit}>
+          <label>
+            Sort by:
+            <select onChange={(e)=>{setSortBy(e.target.value)}}>
+              <option value="created_at">Date</option>
+              <option value="comment_count">Comments</option>
+              <option value="votes">Votes</option>
+            </select>
+            <select onChange={(e)=>{setOrderBy(e.target.value || 'desc')}}>
+              <option value="desc">Descending</option>
+              <option value="asc">Ascending</option>
+            </select>
+          </label>
+  <button onSubmit={handleSubmit}>Submit</button>
+        </form>
 
-
-
-
-          {/* <Link to={`/review/${review.review_id}`}>{review.title}</Link>
-          <br></br>
-          <img
-            src={review.review_img_url}
-            key={review.id}
-            width="200vh"
-            alt={review.title}
-          ></img>
-          <p>Category: {review.category}</p>
-          <p key={review.owner}>Author: {review.owner}</p> */}
-        </>
-      );
-    });
+        <AllReviews allReviews={allReviews}/>
+      </>
+    );
   }
 };
