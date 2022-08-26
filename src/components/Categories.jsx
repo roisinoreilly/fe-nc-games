d408632d708a3ee3c206eb2aa04ef6710133de5d
Roisin O'Reilly <roisin.oreilly1996@gmail.com>
2022-08-26 17:41:11 +0100

    add error handling

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,22 +1,32 @@
 import { useState, useEffect } from "react";
 import { getAllCategories } from "../utils/api";
 import { SingleCategory } from "./SingleCategory";
+import { Error } from "./Error";
 
 const Categories = () => {
     const [allCategories, setAllCategories] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [err, setErr] = useState(false)
 
     useEffect(() => {
         getAllCategories().then((categories) => {
             setAllCategories(categories)
             setIsLoading(false)
         })
+        .catch(() => {
+            setErr(true)
+            setAllCategories([])
+            setIsLoading(false)
+        })
     }, [])
     if (isLoading) {
         return <p>Loading...</p>
-    } else {
+    } 
+    if (err) {
+        return <Error></Error>
+    }
+    else {
     return (allCategories.map((category) => {
-        console.log(category)
             return <SingleCategory category={category}></SingleCategory>
         })
     )
---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+export const Error = () => {
+    return (
+        <>
+    <div id="main">
+    <div class="fof">
+            <h1>Error 404</h1>
+    </div>
+</div>
+        </>
+    )
+}
