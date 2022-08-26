1a00f8817968cce32fa932c3787696a847f3c439
Roisin O'Reilly <roisin.oreilly1996@gmail.com>
2022-08-26 09:59:02 +0100

    add refactored post comment component

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -4,48 +4,90 @@ import moment from "moment";
 
 export const PostComment = ({ review_id }) => {
   const [body, setBody] = useState("");
+  const [message, setMessage] = useState("");
+  const [postedComment, setPostedComment] = useState({});
+  const [err, setErr] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(false);
+
   let username = "jessjelly";
 
   const handleSubmit = (e) => {
     if (body.length < 10 || username === "") {
       e.preventDefault();
-      alert("Comments must be 10 characters or more");
+      return setMessage(
+        <p className="posted-message">Comments must be 10 characters or more</p>
+      );
     } else {
-        document.getElementById("comment-body").disabled = true;
-      postComment(review_id, username, body).then(
-        ( comment ) => {
-            alert("Comment posted")
+      setMessage("");
+      setMessage(<p className="posted-message">Comment posted!</p>);
+      setIsDisabled(true);
+      postComment(review_id, username, body)
+        .then((comment) => {
           return (
             <>
-          <div className="comment-card">
-            <p>Username: {comment.author}</p>
-            <p>{comment.body}</p>
-            <p>Votes: {comment.votes}</p>
-            <p>{moment(comment.created_at).format("LLL")}</p>
-          </div>
-        </>
-          )
-        }
-      );
+              <div className="comment-card">
+                <p>Username: {comment.author}</p>
+                <p>{comment.body}</p>
+                <p>Votes: {comment.votes}</p>
+                <p>{moment(comment.created_at).format("LLL")}</p>
+              </div>
+            </>
+          );
+        })
+        .catch((err) => {
+          setPostedComment({});
+          setErr("Something went wrong, try again");
+        });
       e.preventDefault();
     }
   };
 
-  return (
+  const submitBox = (
     <>
-    <p className="add-comment-label">Add a comment</p>
-    <form className="add-comment" onSubmit={handleSubmit}>
-      <label>
-        <textarea
-          id="comment-body"
-          className="comment-box"
-          type="text"
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
+      <p className="add-comment-label">Add a comment</p>
+      <form className="add-comment" onSubmit={handleSubmit}>
+        <label>
+          <textarea
+            id="comment-body"
+            className="comment-box"
+            type="text"
+            value={body}
+            disabled={isDisabled}
+            onChange={(e) => setBody(e.target.value)}
           ></textarea>
-      </label>
-      <input type="submit" className="submit-button"/>
-    </form>
-          </>
+        </label>
+        <input
+          type="submit"
+          className="submit-button"
+          id="submit-button"
+          disabled={isDisabled}
+        />
+      </form>
+    </>
   );
+
+  if (err)
+    return (
+      <>
+        <p>{err}</p>
+        {submitBox}
+      </>
+    );
+  else
+    return (
+      <>
+        {Object.keys(postedComment).length !== 0 && err === null ? (
+          <div className="comment-card">
+            <p>Username: {postedComment.author}</p>
+            <p>{postedComment.body}</p>
+            <p>Votes: {postedComment.votes}</p>
+            <p>{moment(postedComment.created_at).format("LLL")}</p>
+          </div>
+        ) : (
+          ""
+        )}
+        {message}
+        {submitBox}
+      </>
+    );
 };
---
+++ src/utils/api.js
@@ -0,0 +1,52 @@
+const axios = require("axios")
+
+const api = axios.create({
+    baseURL: "https://roisin-oreilly-nc-games.herokuapp.com/"
+})
+
+export const getAllReviews = (category_slug) => {
+    let queryString = ""
+    if (category_slug !== undefined) {
+        queryString += `?category=${category_slug}`
+    }
+    return api.get(`/api/reviews/${queryString}`).then((res) => {
+        return res.data.reviews
+    })
+}
+
+export const getAllCategories = () => {
+    return api.get("/api/categories").then((res) => {
+        return res.data.categories
+    })
+}
+
+export const getReviewByID = (review_id) => {
+    if (review_id !== undefined) {
+    return api.get(`/api/reviews/${review_id}`).then((res) => {
+        return res.data.review
+    })
+}}
+
+export const increaseVotes = (review_id, votes) => {
+    return api.patch(`api/reviews/${review_id}`, {inc_votes: votes})
+    .then((res) => {
+        return res.data.review.votes
+    })
+}
+
+export const getComments = (review_id) => {
+    return api.get(`api/reviews/${review_id}/comments`)
+    .then((res) => {
+        return res.data.comments
+    })
+}
+
+export const postComment = (review_id, username, body) => {
+    return api.post(`api/reviews/${review_id}/comments`, {username, body})
+    .then((res) => {
+        return res.data.comment
+    })
+    // .catch((error) => {
+    //     console.log(error)
+    // })
+}
