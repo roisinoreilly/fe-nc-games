c397afab90acd03e5058affc33ed929aad710dc8
Roisin O'Reilly <roisin.oreilly1996@gmail.com>
2022-09-04 21:31:50 +0100

    add home text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,12 @@
 const Home = () => {
-    
+    return (
+        <>
+        <p className="home-text">
+            Welcome to NC Games! This is a Reddit-like site for board games built as part of my frontend project at Northcoders.
+        </p>
+        <img className="home-img" src="https://c.pxhere.com/photos/6a/00/blur_board_game_cards_colorful_dice_figures_fun_game-1511745.jpg!d" alt="board games"></img>
+        </>
+    )
 }
 
 export default Home;
